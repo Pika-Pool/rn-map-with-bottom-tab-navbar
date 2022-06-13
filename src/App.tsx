7a337b405a751ffdb1004b43e4aa7ebf41bef727
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2022-06-13 19:45:20 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, Text, useColorScheme } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+	const isDarkMode = useColorScheme() === 'dark';
+
+	const backgroundStyle = {
+		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+	};
+
+	return (
+		<SafeAreaView style={backgroundStyle}>
+			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+			<Text>Hello</Text>
+		</SafeAreaView>
+	);
+}
