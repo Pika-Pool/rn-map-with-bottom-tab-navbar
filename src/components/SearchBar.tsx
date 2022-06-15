59e19a94f1e796a5ed8407d5d4c826c0b7483405
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2022-06-15 12:30:27 +0530

    show search results under search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,33 +1,80 @@
-import React, { useRef } from 'react';
-import { Pressable, StyleSheet, TextInput } from 'react-native';
+import React, { useRef, useState } from 'react';
+import {
+	Pressable,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableHighlight,
+	View,
+} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import useColorTheme from '../hooks/useColorTheme';
 import useThemedValue from '../hooks/useThemedValue';
 import { fontSizes, shadowStyles } from '../styles/global';
+
+const searchResults = ['hello', 'hello Adele', 'population of India', 'PopOS'];
 
 export default function SearchBar() {
 	const textInputRef = useRef<TextInput | null>(null);
 	const { isDarkMode } = useColorTheme();
 	const textColor = useThemedValue('#0d0d0d', '#cccccc');
 
-	/* using pressable to pass press event to text input when the icon is pressed */
+	const [searchText, setSearchText] = useState('');
+	const onSearchResultPress = (text: string) => setSearchText(text);
+
 	return (
-		<Pressable
-			style={[styles.searchBar, isDarkMode ? styles.searchBarDark : null]}
-			onPress={() => textInputRef.current?.focus()}
-		>
-			<MaterialCommunityIcons
-				name='magnify'
-				style={styles.searchIcon}
-				color={textColor}
-			/>
-			<TextInput
-				placeholder='Search Here'
-				placeholderTextColor={textColor}
-				style={[styles.input, isDarkMode ? styles.inputDark : null]}
-				ref={textInputRef}
-			/>
-		</Pressable>
+		<View>
+			{/* using pressable to pass press event to text input when the icon is pressed */}
+			<Pressable
+				style={[styles.searchBar, isDarkMode ? styles.searchBarDark : null]}
+				onPress={() => textInputRef.current?.focus()}
+			>
+				<MaterialCommunityIcons
+					name='magnify'
+					style={styles.searchIcon}
+					color={textColor}
+				/>
+				<TextInput
+					placeholder='Search Here'
+					placeholderTextColor={textColor}
+					style={[styles.input, isDarkMode ? styles.inputDark : null]}
+					ref={textInputRef}
+					value={searchText}
+					onChangeText={text => setSearchText(text)}
+				/>
+			</Pressable>
+
+			{textInputRef.current?.isFocused() ? (
+				<ScrollView
+					style={[
+						styles.searchResultsContainer,
+						isDarkMode ? styles.searchResultsContainerDark : null,
+					]}
+				>
+					{searchResults.map((text, i) => (
+						<TouchableHighlight
+							key={text + i}
+							onPress={() => onSearchResultPress(text)}
+						>
+							<View style={styles.searchResult}>
+								<Text style={[styles.searchResultText, { color: textColor }]}>
+									{text}
+								</Text>
+
+								<TouchableHighlight style={styles.searchResultFillInBtn}>
+									<MaterialCommunityIcons
+										name='arrow-top-left'
+										color={textColor}
+										size={23}
+									/>
+								</TouchableHighlight>
+							</View>
+						</TouchableHighlight>
+					))}
+				</ScrollView>
+			) : null}
+		</View>
 	);
 }
 
@@ -49,9 +96,36 @@ const styles = StyleSheet.create({
 
 	input: {
 		width: '100%',
+		color: '#0d0d0d',
 		...fontSizes.medium,
 	},
 	inputDark: {
 		color: '#cccccc',
+	},
+
+	searchResultsContainer: {
+		backgroundColor: 'white',
+		marginTop: 10,
+		borderRadius: 5,
+		maxHeight: 200,
+	},
+	searchResultsContainerDark: {
+		backgroundColor: '#393939',
+	},
+	searchResult: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: 10,
+	},
+	searchResultText: {
+		paddingRight: 10,
+		color: 'black',
+		maxWidth: '95%',
+		...fontSizes.small,
+	},
+	searchResultFillInBtn: {
+		padding: 5,
 	},
 });
